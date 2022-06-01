c0ef7ea41476a071591d7b9e78d36c8837f89565
BayronZ <bayron.zavalag@gmail.com>
2022-05-31 21:21:42 -0400

    User model with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admins
   get 'home/index'
+  root to: "home#index"
+  authenticate :admin do
+    resources :products
+    resources :categories
+  end
   resources :products
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  
 end
